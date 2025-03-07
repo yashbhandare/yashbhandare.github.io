995773b932639cd2fb2585f50c74bf09d78ac6aa
Yash Bhandare <yashbhandare97@gmail.com>
2025-03-08 01:05:00 +0530

    refactor

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-  output: 'export',
-  trailingSlash: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Necessary for `next export` command to work
+  assetPrefix: './', // Ensures assets load properly from relative paths
+  images: {
+    unoptimized: true, // Needed for Next.js's built-in Image component support
+  },
 };
+
+module.exports = nextConfig;

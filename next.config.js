0aac1ceb1d601d58c28170f4b45e55faa976a993
Yash Bhandare <yashbhandare97@gmail.com>
2025-03-11 22:53:11 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enable static export
-  distDir: "out",
   trailingSlash: true, // Ensures index.html is created in every folder
   basePath: '/yashbhandare.github.io', // This should be your GitHub repo name
   assetPrefix: '/yashbhandare.github.io/',

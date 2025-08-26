d5b0e9e7b77c9d791f7888dcc559b11f05a6c9d7
Yash Bhandare <yashbhandare97@gmail.com>
2025-08-26 19:39:21 +0530

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        es6: true,
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "prettier", // Ensures ESLint doesn't conflict with Prettier formatting rules
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "react-hooks", "jsx-a11y", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:react-hooks/recommended",
-        "airbnb",
-        "prettier", // Ensures ESLint doesn't conflict with Prettier formatting rules
-        "plugin:prettier/recommended"
-    ],
-    plugins: ["react", "react-hooks", "jsx-a11y", "prettier"],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-        react: {
-            version: "detect"
-        }
-    },
-    rules: {
-        "react/prop-types": "off", // Disable prop types rule if using TypeScript
-        "react/react-in-jsx-scope": "off", // Not needed in Next.js or new React versions
-        "no-console": "warn",
-        "no-unused-vars": "warn",
-        "react/jsx-props-no-spreading": "off",
-        "prettier/prettier": "error"
-    }
+  },
+  rules: {
+    "react/prop-types": "off", // Disable prop types rule if using TypeScript
+    "react/react-in-jsx-scope": "off", // Not needed in Next.js or new React versions
+    "no-console": "warn",
+    "no-unused-vars": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "prettier/prettier": "error",
+  },
 };

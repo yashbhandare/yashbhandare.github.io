ec255c3da95067f7b36e7d6a4239dfe04c02d093
Yash Bhandare <yashbhandare97@gmail.com>
2025-08-26 19:48:01 +0530

    rectify contact

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,3 +1,3 @@
 export const FACEBOOK_LINK = "https://www.facebook.com/yashgbhandare97";
-export const EMAIL = "yashgbhandare97@gmail.com";
+export const EMAIL = "yashbhandareproductions@gmail.com";
 export const INSTAGRAM_LINK = "https://www.facebook.com/yashgbhandare97";
